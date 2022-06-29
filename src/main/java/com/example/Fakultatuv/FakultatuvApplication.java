6c9b8bf22b8dd7cfa1e13fad0e7de14b0413f535
vlad2281337 <bi0vladusha@gmail.com>
2022-06-29 14:43:15 +0300

    created a new project

---
+++ src/main/java/com/example/Fakultatuv/FakultatuvApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fakultatuv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakultatuvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakultatuvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Fakultatuv/FakultatuvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fakultatuv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakultatuvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
